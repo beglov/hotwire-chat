72f11186a7528dae7714e87dafc740fa3d000d22
Beglov Sergey <beglov.sergey@yandex.ru>
2021-04-24 17:33:28 +0300

    Use Turbo Frames and Streams

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
   validates :username, length: { minimum: 1, maximum: 20 }
   validates :body, length: { minimum: 1, maximum: 280 }
+
+  after_create_commit -> { broadcast_prepend_to 'posts' }
+  after_update_commit -> { broadcast_replace_to 'posts' }
+  after_destroy_commit -> { broadcast_remove_to 'posts' }
 end
